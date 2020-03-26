7cc54f7d376dc7c02758d40d20140b01c57d696d
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-03-26 16:29:37 +0200

    add name enter functionality

--- frontend/src/WaitingRoom.js
+++ frontend/src/WaitingRoom.js
@@ -1,10 +1,28 @@
 import React from "react";
+import EnterName from "./EnterName";
+
 class WaitingRoom extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isUserEntered: false
+    };
+    this.onNameEnteredHandle = this.onNameEnteredHandle.bind(this);
+  }
+
+  onNameEnteredHandle() {
+    this.setState({ isUserEntered: true });
+  }
+
   render() {
     return (
       <div className="waiting-room">
         <h1>Waiting Room</h1>
-        
+        {this.state.isUserEntered ? null : (
+          <EnterName handler={this.onNameEnteredHandle} />
+        )}
+        <button>Start the game</button>
+        <h2>game id</h2>
       </div>
     );
   }
