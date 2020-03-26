7c0cebcc5f52ad79933b799a2d22bda8e23b8c3e
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-03-26 13:42:43 +0200

    add a phase state to the app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import WelcomeMenu from "./WelcomeMenu";
+const PHASE = {
+  START: 0,
+  WAITING_ROOM: 1,
+  IN_GAME: 2
+};
 class App extends React.Component {
+  constructor() {
+    this.state.phase = PHASE.START;
+  }
+  
   render() {
     return (
       <div className="App">
