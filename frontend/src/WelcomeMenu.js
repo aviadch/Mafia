805a7f913c09ca0ec4b05be0453bb3fff7c87440
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-03-26 18:14:44 +0200

    deleted br and start trying line break without

--- frontend/src/WelcomeMenu.js
+++ frontend/src/WelcomeMenu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import "./WelcomeMenu.css"
 
 class WelcomeMenu extends React.Component {
   render() {
@@ -8,7 +9,7 @@ class WelcomeMenu extends React.Component {
       <div className="welcome-menu">
         <Typography variant="h1" component="h2">Hey - Welcome to Mafia Game</Typography>
         <Button onClick={this.props.newGameHandler} variant="contained" color="primary">Start a New Game</Button>
-        <br/>
+        
         <Button variant="contained" color="primary">Join an Existing Game by ID</Button>
       </div>
     );
