855106869e67d52de5dc24f06673667e35e5eb46
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-03-26 17:37:32 +0200

    Adding material ui buttons

--- frontend/src/WelcomeMenu.js
+++ frontend/src/WelcomeMenu.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
+
 class WelcomeMenu extends React.Component {
   render() {
     return (
       <div className="welcome-menu">
         <h1>Hey - Welcome to Mafia Game</h1>
-        <button onClick={this.props.newGameHandler}>Start a New Game</button>
+        <Button onClick={this.props.newGameHandler} variant="contained">Start a New Game</Button>
         <br/>
-        <button>Join an Existing Game by ID</button>
+        <Button variant="contained">Join an Existing Game by ID</Button>
       </div>
     );
   }
