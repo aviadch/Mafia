f99b01b69c468e8592de7075013b80c188916b66
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-03-26 12:50:33 +0200

    space for prety

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import WelcomeMenu from "./WelcomeMenu"
+import React from "react";
+import WelcomeMenu from "./WelcomeMenu";
 function App() {
   return (
     <div className="App">
-      <WelcomeMenu/>
+      <WelcomeMenu />
     </div>
   );
 }
