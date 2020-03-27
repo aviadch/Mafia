f14312aac1c31baf8e5d53f7a90e1029d0fb08e6
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-03-28 00:29:14 +0300

    Add context consumer to Welcome menu

--- frontend/src/WelcomeMenu.js
+++ frontend/src/WelcomeMenu.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import "./WelcomeMenu.css";
+import { MyContext } from "./MyContext";
 
 class WelcomeMenu extends React.Component {
   render() {
@@ -10,19 +11,24 @@ class WelcomeMenu extends React.Component {
         <Typography variant="h1" component="h2">
           Hey - Welcome to Mafia Game
         </Typography>
-        <div>
-          <Button
-            onClick={this.props.newGameHandler}
-            variant="contained"
-            color="primary"
-          >
-            Start a New Game
-          </Button>
-        </div>
-        <div>
-          <Button variant="contained" color="primary">
-            Join an Existing Game by ID
-          </Button>
+        <MyContext.Consumer>
+          {context => (
+            <React.Fragment>
+              <p>Im the phase {context.state.phase}</p>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+        <div className="buttonContainer">
+          <div>
+            <Button onClick={null} variant="contained" color="primary">
+              Start a New Game
+            </Button>
+          </div>
+          <div>
+            <Button variant="contained" color="primary">
+              Join an Existing Game by ID
+            </Button>
+          </div>
         </div>
       </div>
     );
