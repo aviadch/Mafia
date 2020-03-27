20d9c3d7475b4506024e2818a600771bad174446
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-03-28 00:27:42 +0300

    Create my context file

---
+++ frontend/src/MyContext.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PHASE from "./App";
+export const MyContext = React.createContext();
+
+class MyProvider extends React.Component {
+  state = {
+    phase: PHASE.START
+  };
+
+  componentChoser = () => {
+    let retComp = null;
+    switch (this.state.phase) {
+      case PHASE.START:
+        retComp = <WelcomeMenu />;
+        break;
+
+      case PHASE.WAITING_ROOM:
+        retComp = <WaitingRoom />;
+        break;
+
+      default:
+        retComp = null;
+    }
+    return retComp;
+  };
+
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+export default MyProvider;
