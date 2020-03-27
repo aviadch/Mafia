6d0c28574c6a0d7979f058b34b15bb60f671699e
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-03-27 13:53:01 +0300

    wrap with div instead of br

--- frontend/src/WelcomeMenu.js
+++ frontend/src/WelcomeMenu.js
@@ -1,16 +1,29 @@
 import React from "react";
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import "./WelcomeMenu.css"
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import "./WelcomeMenu.css";
 
 class WelcomeMenu extends React.Component {
   render() {
     return (
       <div className="welcome-menu">
-        <Typography variant="h1" component="h2">Hey - Welcome to Mafia Game</Typography>
-        <Button onClick={this.props.newGameHandler} variant="contained" color="primary">Start a New Game</Button>
-        
-        <Button variant="contained" color="primary">Join an Existing Game by ID</Button>
+        <Typography variant="h1" component="h2">
+          Hey - Welcome to Mafia Game
+        </Typography>
+        <div>
+          <Button
+            onClick={this.props.newGameHandler}
+            variant="contained"
+            color="primary"
+          >
+            Start a New Game
+          </Button>
+        </div>
+        <div>
+          <Button variant="contained" color="primary">
+            Join an Existing Game by ID
+          </Button>
+        </div>
       </div>
     );
   }
