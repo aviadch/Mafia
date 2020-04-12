3f4a3608f7b1f2dde99769a8d0e19970928eb107
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-12 19:45:37 +0300

    Using name prop is now ok

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-const Player = (name) => {
+const Player = (props) => {
+  console.log(props.name);
   return (
     <span className="Player">
-      <h2>{name}</h2>
-      <img src={require("./player1.jpg")} alt={name} height="100" width="100" />
+      <h2>{props.name}</h2>
+      <img
+        src={require("./player1.jpg")}
+        alt={props.name}
+        height="100"
+        width="100"
+      />
     </span>
   );
 };
