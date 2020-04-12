e492b7b72459512415b9c6137f0793820eae1c2f
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-12 19:48:36 +0300

    using context provider right now

--- frontend/src/infra/MyProvider.js
+++ frontend/src/infra/MyProvider.js
@@ -1,27 +1,18 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { MyContext } from "./MyContext";
 import {
   PHASE,
   SERVER_ADDRESS,
   SOCKET_PORT,
-  SERVER_PORT
+  SERVER_PORT,
 } from "../shared_code/consts";
 import Shortid from "shortid";
 import axios from "axios";
 import socketIOClient from "socket.io-client";
 const socket = socketIOClient(SERVER_ADDRESS + ":" + SOCKET_PORT);
 
-class MyProvider extends Component {
-  constructor() {
-    super();
-    socket.on("NewPlayer", data => {
-      this.setState({
-        playerList: data.roomPlayers
-      });
-    });
-  }
-
-  state = {
+const MyProvider = (props) => {
+  const [state, setState] = useState({
     phase: PHASE.WELCOME_SCREEN,
     currentRoom: "",
     isUserEnteredName: false,
@@ -29,85 +20,98 @@ class MyProvider extends Component {
     playerId: "",
     playerList: [],
     joinDate: "",
-    roomCreationDate: ""
-  };
+    roomCreationDate: "",
+  });
 
-  onNewGame = () => {
+  socket.on("NewPlayer", (data) => {
+    console.log("New Player from socket");
+    setState({ ...state, playerList: data.roomPlayers });
+  });
+
+  const onNewGame = () => {
+    console.log("OnNewGame Pressed");
     const playerId = Shortid.generate();
-    this.setState({
-      phase: PHASE.WAITING_ROOM,
-      playerId: playerId
-    });
+
     const req = { creatorID: playerId };
     axios
       .post(SERVER_ADDRESS + ":" + SERVER_PORT + "/room/create", req)
-      .then(res => {
-        const { roomID, a, creationDate } = res.data;
-        this.setState({
+      .then((res) => {
+        const { roomID, ...creationDate } = res.data;
+        console.log(`roomID:${roomID}`);
+        setState({
+          ...state,
+          phase: PHASE.WAITING_ROOM,
+          playerId: playerId,
           currentRoom: roomID,
-          roomCreationDate: creationDate
+          roomCreationDate: creationDate,
         });
+        console.log(state);
       });
+
+    console.log(state);
   };
 
-  joinGame = roomId => {
+  const joinGame = (roomId) => {
     const playerId = Shortid.generate();
-    this.setState({
+    setState({
+      ...state,
       phase: PHASE.WAITING_ROOM,
       currentRoom: roomId,
-      playerId: playerId
+      playerId: playerId,
     });
   };
 
-  onNameEntered = name => {
+  const onNameEntered = (name) => {
     axios
       .get(
         SERVER_ADDRESS +
           ":" +
           SERVER_PORT +
           "/room/join?userID=" +
-          this.state.playerId +
+          state.playerId +
           "&roomID=" +
-          this.state.currentRoom +
+          state.currentRoom +
           "&playerName=" +
           name
       )
-      .then(res => {
+      .then((res) => {
         const { joinDate, roomPlayers, error, errorMessage } = res.data;
         if (error) {
           console.log(errorMessage);
         } else {
-          this.setState({
+          setState({
+            ...state,
             isUserEnteredName: true,
             playerName: name,
             joinDate: joinDate,
-            playerList: [...roomPlayers]
+            playerList: [...roomPlayers],
           });
         }
       });
   };
 
-  setName = name => {
-    this.setState({
-      playerName: name
+  const setName = (name) => {
+    setState({
+      ...state,
+      playerName: name,
     });
   };
 
-  render() {
-    return (
+  return (
+    <div>
       <MyContext.Provider
         value={{
-          state: this.state,
-          onNewGame: this.onNewGame,
-          onNameEntered: this.onNameEntered,
-          joinGame: this.joinGame,
-          setName: this.setName
+          state: state,
+          onNewGame: onNewGame,
+          onNameEntered: onNameEntered,
+          joinGame: joinGame,
+          setName: setName,
         }}
       >
-        {this.props.children}
+        {props.children}
       </MyContext.Provider>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default MyProvider;
