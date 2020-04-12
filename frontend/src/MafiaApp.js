234702b6da33e6a60253724be9064f46bacfdcb8
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-12 23:39:13 +0300

    screen chooser no longer needed

--- frontend/src/MafiaApp.js
+++ frontend/src/MafiaApp.js
@@ -8,31 +8,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const MafiaApp = () => {
   const context = useContext(MyContext);
-  // const screenChooser = (phase) => {
-  // let screen = null;
-  //   switch (phase) {
-  //     case PHASE.WELCOME_SCREEN:
-  //       console.log(phase);
-  //       screen = <WelcomeScreen />;
-  //       break;
-
-  //     case PHASE.WAITING_ROOM:
-  //       console.log(phase);
-  //       screen = <WaitingRoom />;
-  //       break;
-
-  //     default:
-  //       screen = null;
-  //   }
-  //   return screen;
-  // };
-
-  // let screen = screenChooser(context.state.phase);
   return (
     <div className="MafiaApp">
       <Switch>
         <Route path="/room" component={WaitingRoom}></Route>
-
         <Route path="/" component={WelcomeScreen}></Route>
       </Switch>
     </div>
