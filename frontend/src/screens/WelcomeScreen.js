8a3f2285f02fd9f59dded4199c4aeb28305e84fa
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-12 16:36:22 +0300

    change to func and remove state to useState

--- frontend/src/screens/WelcomeScreen.js
+++ frontend/src/screens/WelcomeScreen.js
@@ -1,63 +1,58 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import "./WelcomeScreen.css";
 import { MyContext } from "../infra/MyContext";
 import TextField from "@material-ui/core/TextField";
 
-class WelcomeScreen extends Component {
-  constructor() {
-    super();
-    this.state = {
-      roomToJoin: ""
-    };
-  }
-  onChange = event => {
-    const value = event.target.value;
-    console.log(this.state.roomToJoin);
-    this.setState({ roomToJoin: value });
-  };
-  render() {
-    return (
-      <div className="welcome-screen">
-        <Typography variant="h1" component="h2">
-          Hey - Welcome to Mafia Game
-        </Typography>
+const WelcomeScreen = () => {
+  const [roomToJoin, setRoom] = useState("");
 
-        <div className="buttonContainer">
-          <MyContext.Consumer>
-            {context => (
-              <>
-                <div>
-                  <Button
-                    onClick={context.onNewGame}
-                    variant="contained"
-                    color="primary"
-                  >
-                    Start a New Game
-                  </Button>
-                </div>
-                <div>
-                  <TextField
-                    onChange={this.onChange}
-                    id="standard-basic"
-                    label="RoomId"
-                  />
-                  <Button
-                    onClick={() => context.joinGame(this.state.roomToJoin)}
-                    variant="contained"
-                    color="primary"
-                  >
-                    Join Game
-                  </Button>
-                </div>
-              </>
-            )}
-          </MyContext.Consumer>
-        </div>
+  const onChange = (event) => {
+    const value = event.target.value;
+    setRoom(value);
+    console.log(roomToJoin);
+  };
+
+  return (
+    <div className="welcome-screen">
+      <Typography variant="h1" component="h2">
+        Hey - Welcome to Mafia Game
+      </Typography>
+
+      <div className="buttonContainer">
+        <MyContext.Consumer>
+          {(context) => (
+            <>
+              <div>
+                <Button
+                  onClick={context.onNewGame}
+                  variant="contained"
+                  color="primary"
+                >
+                  Start a New Game
+                </Button>
+              </div>
+              <div>
+                <TextField
+                  onChange={onChange}
+                  id="standard-basic"
+                  label="RoomId"
+                />
+                <Button
+                  onClick={() => context.joinGame(roomToJoin)}
+                  variant="contained"
+                  color="primary"
+                >
+                  Join Game
+                </Button>
+              </div>
+            </>
+          )}
+        </MyContext.Consumer>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default WelcomeScreen;
