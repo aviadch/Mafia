b3b5f0b6eea8be8c35697a15da0e239b4ec8d76b
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-12 16:43:57 +0300

    enter name is now a function

--- frontend/src/components/EnterName.js
+++ frontend/src/components/EnterName.js
@@ -1,45 +1,38 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { MyContext } from "../infra/MyContext.js";
-class EnterName extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: ""
-    };
-  }
+const EnterName = () => {
+  const [name, setName] = useState("");
 
-  handleOnChange = event => {
-    this.setState({ name: event.target.value });
+  const handleOnChange = (event) => {
+    setName(event.target.value);
   };
 
-  render() {
-    return (
-      <MyContext.Consumer>
-        {context => (
-          <>
-            <div className="Enter Name">
-              <TextField
-                onChange={this.handleOnChange}
-                id="standard-basic"
-                label="Enter Your Name"
-              />
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => {
-                  return context.onNameEntered(this.state.name);
-                }}
-              >
-                Submit
-              </Button>
-            </div>
-          </>
-        )}
-      </MyContext.Consumer>
-    );
-  }
-}
+  return (
+    <MyContext.Consumer>
+      {(context) => (
+        <>
+          <div className="Enter Name">
+            <TextField
+              onChange={handleOnChange}
+              id="standard-basic"
+              label="Enter Your Name"
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                return context.onNameEntered(name);
+              }}
+            >
+              Submit
+            </Button>
+          </div>
+        </>
+      )}
+    </MyContext.Consumer>
+  );
+};
 
 export default EnterName;
