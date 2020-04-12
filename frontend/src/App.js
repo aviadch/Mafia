56f9a75fc943e9b8faddd9c1822552a99f978813
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-12 19:44:53 +0300

    composite the app to mafia app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,13 @@
 import React from "react";
 
-import WelcomeScreen from "./screens/WelcomeScreen";
-import WaitingRoom from "./screens/WaitingRoom";
-import { MyContext } from "./infra/MyContext";
 import MyProvider from "./infra/MyProvider.js";
-import { PHASE } from "./shared_code/consts.js";
+
+import MafiaApp from "./MafiaApp";
 
 const App = () => {
-  const screenChooser = (phase) => {
-    let screen = null;
-    switch (phase) {
-      case PHASE.WELCOME_SCREEN:
-        screen = <WelcomeScreen />;
-        break;
-
-      case PHASE.WAITING_ROOM:
-        screen = <WaitingRoom />;
-        break;
-
-      default:
-        screen = null;
-    }
-    return screen;
-  };
-
   return (
     <MyProvider>
-      <div className="App">
-        <MyContext.Consumer>
-          {(context) => {
-            const screen = screenChooser(context.state.phase);
-            return <>{screen}</>;
-          }}
-        </MyContext.Consumer>
-      </div>
+      <MafiaApp />
     </MyProvider>
   );
 };
---
+++ frontend/src/MafiaApp.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+
+import WelcomeScreen from "./screens/WelcomeScreen";
+import WaitingRoom from "./screens/WaitingRoom";
+import { MyContext } from "./infra/MyContext";
+import { PHASE } from "./shared_code/consts.js";
+
+const MafiaApp = () => {
+  const context = useContext(MyContext);
+  const screenChooser = (phase) => {
+    let screen = null;
+    switch (phase) {
+      case PHASE.WELCOME_SCREEN:
+        console.log(phase);
+        screen = <WelcomeScreen />;
+        break;
+
+      case PHASE.WAITING_ROOM:
+        console.log(phase);
+        screen = <WaitingRoom />;
+        break;
+
+      default:
+        screen = null;
+    }
+    return screen;
+  };
+
+  let screen = screenChooser(context.state.phase);
+  return <div className="MafiaApp">{screen}</div>;
+};
+
+export default MafiaApp;
