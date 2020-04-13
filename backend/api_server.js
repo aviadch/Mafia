e120951b552fa51637689a74a8275aea35777ae4
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-13 23:56:30 +0300

    change to const

--- backend/api_server.js
+++ backend/api_server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
 
-var corsOptions = {
+const corsOptions = {
   origin: "http://localhost:3000",
 };
 app.use(cors(corsOptions));
