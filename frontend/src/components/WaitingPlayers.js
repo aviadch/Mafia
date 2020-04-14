d468dff02c20cc17283305b229b407eb9d2db205
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-14 12:30:54 +0300

    changed for bug mistakes

--- frontend/src/components/WaitingPlayers.js
+++ frontend/src/components/WaitingPlayers.js
@@ -6,11 +6,11 @@ class WaitingPlayers extends Component {
   render() {
     return (
       <MyContext.Consumer>
-        {context => {
+        {(context) => {
           return (
             <span className="waiting-players">
               <h1>Waiting Players</h1>
-              {context.state.playerList.map(player => (
+              {context.state.roomPlayersList.map((player) => (
                 <Player name={player["name"]} key={player["id"]} />
               ))}
             </span>
