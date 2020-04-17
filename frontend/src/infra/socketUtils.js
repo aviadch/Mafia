72c56b70e196a89d7e8511d32bb0866e2f33fb4b
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-17 16:43:22 +0300

    Revert "eslint changes"
    This reverts commit e59fec422145fbc7de310b0ca7218bac5ceeb492.

--- frontend/src/infra/socketUtils.js
+++ frontend/src/infra/socketUtils.js
@@ -1,9 +1,10 @@
-import socketIOClient from 'socket.io-client';
 import { SERVER_ADDRESS } from '../shared_code/consts';
-
+import socketIOClient from 'socket.io-client';
 let socket = null;
 
-export const createSocket = (socketPort) => socketIOClient(`${SERVER_ADDRESS}:${socketPort}`);
+export const createSocket = (socketPort) => {
+  return socketIOClient(`${SERVER_ADDRESS}:${socketPort}`);
+};
 
 export const verifySocketListen = (socket, evnetStr, callback) => {
   if (!socket._callbacks[evnetStr]) {
