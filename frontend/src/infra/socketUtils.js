e59fec422145fbc7de310b0ca7218bac5ceeb492
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-17 16:28:03 +0300

    eslint changes

--- frontend/src/infra/socketUtils.js
+++ frontend/src/infra/socketUtils.js
@@ -1,10 +1,9 @@
-import { SERVER_ADDRESS } from '../shared_code/consts';
 import socketIOClient from 'socket.io-client';
+import { SERVER_ADDRESS } from '../shared_code/consts';
+
 let socket = null;
 
-export const createSocket = (socketPort) => {
-  return socketIOClient(`${SERVER_ADDRESS}:${socketPort}`);
-};
+export const createSocket = (socketPort) => socketIOClient(`${SERVER_ADDRESS}:${socketPort}`);
 
 export const verifySocketListen = (socket, evnetStr, callback) => {
   if (!socket._callbacks[evnetStr]) {
