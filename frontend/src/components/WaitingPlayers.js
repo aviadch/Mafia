f0b8269ad7328dd4c405052a7b7efeacb91fccf1
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-17 17:14:56 +0300

    eslint changes to waitingPlayers

--- frontend/src/components/WaitingPlayers.js
+++ frontend/src/components/WaitingPlayers.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
 import PlayerCard from './PlayerCard';
-import { MyContext } from '../infra/MyContext.js';
+import { MyContext } from '../infra/MyContext';
 
 const WaitingPlayers = () => {
   const context = useContext(MyContext);
+  const { room } = context;
   return (
     <span className="waiting-players">
       <h1>Waiting Players</h1>
-      {context.room.playersList.map((player) => {
-        return <PlayerCard name={player.name} key={player.id} />;
-      })}
+      {room.playersList.map((player) => (
+        <PlayerCard name={player.name} key={player.id} />
+      ))}
     </span>
   );
 };
