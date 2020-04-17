1e4d68bb0355d9e388f8434a6adfe7850bee8901
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-17 17:59:56 +0300

    Mafia routes eslint changes

--- frontend/src/MafiaRoutes.js
+++ frontend/src/MafiaRoutes.js
@@ -1,19 +1,17 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 import WelcomeScreen from './screens/WelcomeScreen';
 import WaitingRoom from './screens/WaitingRoom';
-import { Switch, Route } from 'react-router-dom';
+
 import { ROOM_ROUTES } from './shared_code/consts';
 
-const MafiaRoutes = () => {
-  return (
-    <div className="MafiaApp">
-      <Switch>
-        <Route path={`/${ROOM_ROUTES}`} component={WaitingRoom}></Route>
-        <Route path="/" component={WelcomeScreen}></Route>
-      </Switch>
-    </div>
-  );
-};
+const MafiaRoutes = () => (
+  <div className="MafiaApp">
+    <Switch>
+      <Route path={`/${ROOM_ROUTES}`} component={WaitingRoom} />
+      <Route path="/" component={WelcomeScreen} />
+    </Switch>
+  </div>
+);
 
 export default MafiaRoutes;
