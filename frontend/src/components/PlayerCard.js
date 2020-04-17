d7e1f2a4b0837f2c9288b782af524b0f114de945
Aviad Chmelnik <aviadchmelnik@gmail.com>
2020-04-17 17:05:27 +0300

    Player Card eslint changes

--- frontend/src/components/PlayerCard.js
+++ frontend/src/components/PlayerCard.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const playerImage = require('./player1.jpg');
 
 const PlayerCard = (props) => {
+  const { name } = props;
   return (
     <span className="Player">
-      <h2>{props.name}</h2>
-      <img
-        src={require('./player1.jpg')}
-        alt={props.name}
-        height="100"
-        width="100"
-      />
+      <h2>{name}</h2>
+      <img src={playerImage} alt={name} height="100" width="100" />
     </span>
   );
+};
+
+PlayerCard.propTypes = {
+  name: PropTypes.string.isRequired,
 };
 
 export default PlayerCard;
